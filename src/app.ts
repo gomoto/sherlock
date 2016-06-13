54f4e5c12f2a6a4f598471a433804ecbfe3c36c4
Ryan Gomoto <ryan@cyrusbio.com>
2016-06-13 01:27:22 -0700

    Create brain component and display all tags and all notes with no tags

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as angular from 'angular';
 import * as uiRouter from 'angular-ui-router';
 import routing from './app.routing';
 
+import brain from './brain/brain.module';
 import janitor from './janitor/janitor.module';
 import navbar from './navbar/navbar.module';
 import note from './note/note.module';
@@ -9,6 +10,7 @@ import pouchdb from './pouchdb/pouchdb.module';
 
 angular.module('sherlock', [
   uiRouter as string,
+  brain,
   janitor,
   navbar,
   note,
