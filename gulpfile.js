1a42587a37f0f20d940c41e7f1a148fcb3d39f14
Ryan Gomoto <ryan@cyrusbio.com>
2016-06-18 01:42:52 -0700

    Move ts tasks to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,38 @@
 var autoprefixer = require('gulp-autoprefixer');
+var browserify = require('browserify');
 var fs = require('fs');
 var gulp = require('gulp');
 var inject = require('html-injector');
 var log = require('gulp-util').log;
 var minify = require('html-minifier').minify;
+var path = require('path');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var trash = require('trash');
+var tsify = require('tsify');
+
 
 var config = require('./config.json');
 
 var minifyHtml = () => {
-  var content = fs.readFileSync(config.html, {encoding: 'utf-8'});
+  var content = fs.readFileSync(config.dist.html, {encoding: 'utf-8'});
   var minified = minify(content, {
     collapseWhitespace: true,
     processScripts: ['text/ng-template']
   });
-  fs.writeFileSync(config.html, minified);
+  fs.writeFileSync(config.dist.html, minified);
 };
 
 gulp.task('html', function() {
   inject(config.entry.html)
-  .replace('css', config.css)
-  .replace('js', config.bundle) // config.bundlemin
-  .replace('template', 'src/**/*.html')
-  .write(config.html, minifyHtml);
+  .replace('css', config.dist.css)
+  .replace('js', config.dist.js)
+  .replace('template', config.src.html)
+  .write(config.dist.html, minifyHtml);
 });
 
 gulp.task('html:clean', function() {
-  trash([config.html]);
+  trash([config.dist.html]);
 });
 
 gulp.task('sass', function() {
@@ -38,11 +42,11 @@ gulp.task('sass', function() {
   .pipe(sass(config.options.sass).on('error', sass.logError))
   .pipe(autoprefixer(config.options.autoprefixer))
   .pipe(sourcemaps.write())
-  .pipe(gulp.dest('build'));
+  .pipe(gulp.dest(path.dirname(config.dist.css)));
 });
 
 gulp.task('sass:clean', function() {
-  trash([config.css])
+  trash([config.dist.css])
 });
 
 gulp.task('sass:watch', function() {
@@ -51,4 +55,19 @@ gulp.task('sass:watch', function() {
   .on('change', function(event) {
     console.log('File ' + event.path + ' ' + event.type);
   });
+});
+
+gulp.task('ts', function() {
+  browserify(config.options.browserify)
+  .add(config.entry.ts)
+  .plugin(tsify, config.options.tsify)
+  .bundle()
+  .on('error', (error) => {
+    console.error(error.toString());
+  })
+  .pipe(fs.createWriteStream(config.dist.js));
+});
+
+gulp.task('ts:clean', function() {
+  trash([config.dist.js]);
 });
