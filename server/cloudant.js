42eb01dd8714ecaed1ea1e5ea27e7b0bce2ee0f3
Ryan Gomoto <ryan@cyrusbio.com>
2016-06-27 00:06:47 -0700

    Move Cloudant code to separate module

---
+++ server/cloudant.js
@@ -0,0 +1,41 @@
+var Cloudant = require('cloudant');
+
+var cloudant = Cloudant({
+  account: process.env.CLOUDANT_ACCOUNT,
+  password: process.env.CLOUDANT_PASSWORD
+}, (error, cloudant) => {
+  if (error) return console.error('Failed to initialize Cloudant: ' + error.message);
+  console.log('Cloudant initialized');
+});
+
+module.exports = {
+  // Create Cloudant database for the specified Stormpath user
+  createDatabase: (user) => {
+    // create database
+    cloudant.db.create(user.username, (error) => {
+      if (error) return console.error('Error creating Cloudant database for ' + user.username, error);
+      console.log('Created Cloudant database for ' + user.username);
+      // generate api key
+      cloudant.generate_api_key((error, api) => {
+        if (error) return console.error('Error generating Cloudant API key for ' + user.username, error);
+        console.log('Generated Cloudant API key for ' + user.username);
+        // set database security for api key
+        var security = {};
+        security['nobody'] = [];
+        security[api.key] = ['_replicator'];
+        var db = cloudant.db.use(user.username);
+        db.set_security(security, (error, result) => {
+          if (error) return console.error('Error setting Cloudant security for ' + user.username, error);
+          console.log('Set Cloudant security for ' + user.username);
+          // save api key to stormpath custom data
+          user.customData.cloudant_key = api.key;
+          user.customData.cloudant_password = api.password;
+          user.customData.save((error) => {
+            if (error) return console.error('Error saving Cloudant API key to Stormpath customData for ' + user.username, error);
+            console.log('Saved Cloudant API key to Stormpath customData');
+          });
+        });
+      });
+    });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 var bodyParser = require('body-parser');         // pull information from HTML POST (express4)
-var Cloudant = require('cloudant');
+var cloudant = require('./cloudant');
 var express = require('express');
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var morgan = require('morgan');
@@ -10,16 +10,6 @@ var stormpath = require('express-stormpath');
 var winston = require('winston');
 
 var indexHtml = path.join(process.cwd(), 'index.html');
-
-var cloudant = Cloudant({
-  account: process.env.CLOUDANT_ACCOUNT,
-  password: process.env.CLOUDANT_PASSWORD
-}, (error, cloudant) => {
-  if (error) {
-    return console.error('Failed to initialize Cloudant: ' + error.message);
-  }
-  console.log('Cloudant initialized');
-});
 
 var app = express();
 
@@ -45,46 +35,8 @@ app.use(stormpath.init(app, {
     customData: true,
   },
   postRegistrationHandler: (account, req, res, next) => {
-
-    // create database
-    cloudant.db.create(account.username, function(error) {
-      if (error) {
-        console.error('Error creating Cloudant database for ' + account.username, error);
-        return;
-      }
-      console.log('Created Cloudant database for ' + account.username);
-      // generate api key
-      cloudant.generate_api_key(function(error, api) {
-        if (error) {
-          console.error('Error generating Cloudant API key for ' + account.username, error);
-        }
-        console.log('Generated Cloudant API key for ' + account.username);
-        // set database security for api key
-        var security = {};
-        security['nobody'] = [];
-        security[api.key] = ['_replicator'];
-        var db = cloudant.db.use(account.username);
-        db.set_security(security, function(error, result) {
-          if (error) {
-            console.error('Error setting Cloudant security for ' + account.username, error);
-            return;
-          }
-          console.log('Set Cloudant security for ' + account.username);
-          // save api key to stormpath custom data
-          req.user.customData.cloudant_key = api.key;
-          req.user.customData.cloudant_password = api.password;
-          req.user.customData.save(function (error) {
-            if (error) {
-              console.error('Error saving Cloudant API key to Stormpath customData for ' + account.username, error);
-            }
-            console.log('Saved Cloudant API key to Stormpath customData');
-            // DONE!
-          });
-        });
-      });
-    });
-
-    // Don't wait for database to be created
+    // asynchronously create database
+    cloudant.createDatabase(req.user);
     next();
   },
   web: {
